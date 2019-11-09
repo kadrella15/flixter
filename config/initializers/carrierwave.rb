bbe9d3670e96afccd4651daba20a0b549b208fc3
kadrella <kadrellall@icloud.com>
2019-11-09 21:10:18 +0000

    Lesson 24: time issue

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,11 +2,13 @@
 
 CarrierWave.configure do |config|
   config.storage = 'aws'                        # required
+  config.aws_bucket  = ENV["AWS_BUCKET"]        # required
+  config.aws_acl = "public-read"
+
   config.fog_credentials = {
     provider:              'AWS',                        # required
     aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
     aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
-    region: ENV["AWS_REGION"]
+    region:                ENV["AWS_REGION"]
   }
-  config.aws_bucket  = ENV["AWS_BUCKET"]              # required
 end
